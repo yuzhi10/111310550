2bfcebf97bc54f631168a10fe4b12125f1c94ace
yuzhi10 <s111310550@student.nqu.edu.tw>
2025-04-29 18:36:13 +0800

    Create 05.py

---
+++ homework/HW8/05.py
@@ -0,0 +1,13 @@
+def grade(score):
+    if 90 <= score <= 100:
+        return 'A'
+    elif 80 <= score <= 89:
+        return 'B'
+    elif 70 <= score <= 79:
+        return 'C'
+    elif 60 <= score <= 69:
+        return 'D'
+    elif 0 <= score <= 59:
+        return 'F'
+    else:
+        return 'Invalid score'
