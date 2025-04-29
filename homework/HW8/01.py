d4eb4933a76f3d37173ec377cc520c36dad94212
yuzhi10 <s111310550@student.nqu.edu.tw>
2025-04-29 18:30:20 +0800

    Create 01.py

---
+++ homework/HW8/01.py
@@ -0,0 +1,10 @@
+def find_max(numbers):
+    if not numbers:
+        return None
+    max_num = numbers[0]
+    for num in numbers[1:]:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+print(find_max([1, 5, 3, 9, 2]))
